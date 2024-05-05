9af5750c672c2cff06d554bc5eabcbf39612d647
Erzhan <saralaev03@mail.ru>
2024-05-05 15:50:58 +0600

    Add URLSessin/getData

--- ST-HW15-Саралаев Ержан/ViewController.swift
+++ ST-HW15-Саралаев Ержан/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData(urlRequest: "https://cat-fact.herokuapp.com/facts")
     }
-
-
+ 
+    func getData(urlRequest: String) {
+        let urlRequest = URL(string: urlRequest)
+        guard let url = urlRequest else {
+            print("Ошибка: Неккоректная ссылка")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Ошибка: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Ошибка: Некорректный ответ сервера")
+                return
+            }
+            
+            print("Код ответа от сервера: \(httpResponse.statusCode)")
+            
+            guard let data = data else {
+                print("Ошибка: Отсутствуют данные")
+                return
+            }
+            
+            if let dataAsString = String(data: data, encoding: .utf8) {
+                print("Данные, пришедшие с сервера: \(dataAsString)")
+            } else {
+                print("Ошибка: не удалось преоброзовать данные в строку")
+            }
+            
+        }.resume()
+    }
+    
 }
-
